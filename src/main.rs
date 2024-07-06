0aa01718bc2942771b7b44a59c7a4459f6950c3d
Rudi Blaha Svartveit <25234080+rudsvar@users.noreply.github.com>
2024-07-07 00:17:45 +0200

    Allow reading from url, path or stdin

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
 use clap::Parser;
+use reqwest::Url;
 
 #[derive(clap::Parser)]
 struct Args {
-    path: String,
+    /// URL or file path. If not provided, reads from stdin.
+    uri: Option<String>,
+    /// Choose beans to include by package.
     #[clap(short, long)]
     package_filter: Option<String>,
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let args: Args = Args::parse();
-    let path = args.path;
     let package_filter = args.package_filter.unwrap_or_default();
-    let content = std::fs::read_to_string(path).unwrap();
-    let top_level: mermaid_beans::model::ContextWrapper = serde_json::from_str(&content).unwrap();
+    let content = match args.uri {
+        Some(uri) => match Url::parse(&uri) {
+            Ok(url) => reqwest::blocking::get(url)?.text()?,
+            Err(_) => std::fs::read_to_string(uri)?,
+        },
+        None => std::io::read_to_string(std::io::stdin())?,
+    };
+    let top_level: mermaid_beans::model::ContextWrapper = serde_json::from_str(&content)?;
     let mermaid = mermaid_beans::generator::generate_mermaid(top_level, &package_filter);
     println!("{}", mermaid);
+    Ok(())
 }
