af149bce6361b3017ae952e23291df3b9084839a
Rudi Blaha Svartveit <25234080+rudsvar@users.noreply.github.com>
2024-07-07 00:00:31 +0200

    Add clap for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+use clap::Parser;
+
+#[derive(clap::Parser)]
+struct Args {
+    path: String,
+    #[clap(short, long)]
+    package_filter: Option<String>,
+}
+
 fn main() {
-    let path = std::env::args().nth(1).unwrap();
-    let package_filter = std::env::args().nth(2).unwrap_or("".to_string());
+    let args: Args = Args::parse();
+    let path = args.path;
+    let package_filter = args.package_filter.unwrap_or_default();
     let content = std::fs::read_to_string(path).unwrap();
     let top_level: mermaid_beans::model::ContextWrapper = serde_json::from_str(&content).unwrap();
     let mermaid = mermaid_beans::generator::generate_mermaid(top_level, &package_filter);
