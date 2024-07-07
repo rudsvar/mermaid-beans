79ff0cd8bfdd60250cd2e1f478382459157eb50f
Rudi Blaha Svartveit <25234080+rudsvar@users.noreply.github.com>
2024-07-07 14:26:03 +0200

    Update lock file

--- release.sh
+++ release.sh
@@ -2,6 +2,9 @@
 
 set -e
 
-cargo bump "$1" -g
-git push
-git push --tags
+cargo bump "$1"
+cargo generate-lockfile
+git add .
+git commit -m "Release $(cargo metadata --format-version=1 --no-deps | jq '.packages[0].version' -r)"
+# git push
+# git push --tags
