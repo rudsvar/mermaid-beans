79ee6b5ce86fc14c7ff0f3924fd42a394ecd4a02
Rudi Blaha Svartveit <25234080+rudsvar@users.noreply.github.com>
2024-07-07 14:27:34 +0200

    Update release script again

--- release.sh
+++ release.sh
@@ -3,8 +3,10 @@
 set -e
 
 cargo bump "$1"
+VERSION=$(cargo metadata --format-version=1 --no-deps | jq '.packages[0].version' -r)
 cargo generate-lockfile
 git add .
-git commit -m "Release $(cargo metadata --format-version=1 --no-deps | jq '.packages[0].version' -r)"
-# git push
-# git push --tags
+git commit -m "Release version ${VERSION}"
+git tag "${VERSION}"
+git push
+git push --tags
